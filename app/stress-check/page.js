caaa04c96eb0211bec68cae0a2a9d0ab41a0ab69
Harshit-Github16 <your.email@example.com>
2026-01-06 19:08:29 +0530

    c

--- app/stress-check/page.js
+++ app/stress-check/page.js
@@ -489,6 +489,7 @@ export default function StressCheckPage() {
                     </div>
                 </div>
 
+
                 <BottomNav activePage="stress-check" />
             </div>
         );
